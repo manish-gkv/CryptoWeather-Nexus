e965a3210de4315097f3f33c2c550236697a06af
manish-gkv <manishkumarslmp@gmail.com>
2025-04-03 00:12:09 +0530

    added go-to-dashboard button on home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,18 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 export default function Page(){
-	return (
-			<h1>Crypto Weather Nexus</h1>
-		)
+  const router = useRouter();
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <button
+        onClick={() => router.push('/dashboard')}
+        className="bg-primary-green text-white px-8 py-4 rounded-xl shadow-lg hover:bg-[#31513f] transition-all duration-200 text-2xl font-semibold hover:shadow-xl hover:scale-105 active:scale-95"
+      >
+        Go to Dashboard
+      </button>
+    </div>
+  );
 }
