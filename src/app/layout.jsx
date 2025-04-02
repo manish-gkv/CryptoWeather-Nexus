3ddb2f4f073b88609ceff01b5dcfb93ef7e57899
manish-gkv <manishkumarslmp@gmail.com>
2025-04-03 00:31:36 +0530

    added analytics components into layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,7 @@ export default function DashboardLayout({ children }) {
   return (
     <html lang="en">
       <body className={'bg-gray-50'}>
+        <Analytics/>
         <Header /> 
         <main className={'mt-25 px-5 pb-10 lg:px-20 lg:pb-20'}>{children}</main>
       </body>
